2ceac173fdeb01a121be4a5bc8c3d09a063f6555
adamus.r <adamus.r@gmail.com>
2020-06-17 20:22:09 +0200

    inital test class

---
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/atm/ATMachineTest.java
@@ -0,0 +1,19 @@
+package edu.iis.mto.testreactor.atm;
+
+import static org.junit.Assert.assertThat;
+
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ATMachineTest {
+
+    @Before
+    public void setUp() throws Exception {}
+
+    @Test
+    public void itCompiles() {
+        assertThat(true, Matchers.equalTo(true));
+    }
+
+}
