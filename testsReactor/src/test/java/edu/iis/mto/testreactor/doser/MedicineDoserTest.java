7b493d7864fc6ee8986f11948fd9a03034625fbb
adamus.r <adamus.r@gmail.com>
2020-09-15 10:51:30 +0200

    JU5 test

--- testsReactor/src/test/java/edu/iis/mto/testreactor/doser/MedicineDoserTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/doser/MedicineDoserTest.java
@@ -1,30 +1,16 @@
 package edu.iis.mto.testreactor.doser;
 
-import java.util.concurrent.TimeUnit;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import edu.iis.mto.testreactor.doser.infuser.Infuser;
-
 @ExtendWith(MockitoExtension.class)
-public class MedicineDoserTest {
-
-    @Mock
-    private Infuser infuser;
-    @Mock
-    private Clock clock;
-    @Mock
-    private DosageLog dosageLog;
+class MedicineDoserTest {
 
     @Test
-    void test() {
-        MedicineDoser doser = new MedicineDoser(infuser, dosageLog, clock);
-        doser.add(MedicinePackage.of(Medicine.of("betaBlocker"), Capacity.of(1, CapacityUnit.LITER)));
-        doser.dose(
-                Receipe.of(Medicine.of("betaBlocker"), Dose.of(Capacity.of(10, CapacityUnit.MILILITER), Period.of(1, TimeUnit.HOURS)), 10));
-
+    void itCompiles() {
+        assertEquals(2, 1 + 1);
     }
 }
